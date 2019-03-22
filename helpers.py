6fbb90c263459258cacd3b8994e07e11552fbad9
philip_owen <owen.philipb@gmail.com>
2019-03-22 10:22:54 -0500

    added gen_hosts function to auto-generate host files based on inventory

--- helpers.py
+++ helpers.py
@@ -6,3 +6,11 @@ def createGroupedDict(group_params, host_params):
 def handleMissingDevice(device):
     print("Cannot find file %s.json..." % device)
     print("Skipping device...")
+
+def gen_hosts():
+    f = open('inventory.txt')
+    i = f.readlines()
+    for device in i:
+        device = device.rstrip()
+        file = open('./hosts/' + device + '.json', 'w')
+        file.close()
