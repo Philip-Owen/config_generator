794acc849e357e5a7cf0d150ad5e9f2a0ad226fc
philip_owen <owen.philipb@gmail.com>
2019-04-05 10:24:32 -0500

    added logic to prevent host files being overwritten if they already exist

--- helpers.py
+++ helpers.py
@@ -1,22 +1,28 @@
 import json
 import os
+from pathlib import Path
+
 
 def createGroupedDict(group_params, host_params):
     merged = group_params.copy()
     merged.update(host_params)
     return merged
 
+
 def handleMissingDevice(device):
     print("Cannot find file %s.json..." % device)
     print("Skipping device...")
 
+
 def gen_hosts():
-    f = open('inventory.txt')
-    i = f.readlines()
-    for device in i:
-        device = device.rstrip()
-        file = open('./hosts/' + device + '.json', 'w')
-        file.close()
+	f = open('inventory.txt')
+	i = f.readlines()
+	for device in i:
+		device = device.rstrip()
+		file_path = Path('./hosts/' + device + '.json')
+		if not file_path.is_file():
+			file = open(file_path, 'w')
+			file.close()
 
 def loadGroupParms(path):
     group = {}
